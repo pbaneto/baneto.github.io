e5a7ee951b6a2209497ace3de3ba49b48ec14bf7
paula22 <paula@vionlabs.com>
2023-04-20 22:58:55 +0200

    texto contacto

--- src/Contacto.js
+++ src/Contacto.js
@@ -6,16 +6,27 @@ export default function Contacto() {
         <div>
             <NavBar />
             <div className='container-fluid contacto-all'>
-                <div className='row'>
-                    <div className="contacto">
-                        <img src={require('./photos/fondo_contacto.jpg')} className='contacto-img' alt={'img-contacto'}></img>
-                    </div>
-                    <div className='sobre'>
-                        <b>Sobre Baneto</b>
-                        <p>
-                            Me llamo Paula y me gustan los pendientes. 
-                        </p>
-                    </div>
+                <div className="contacto">
+                    <img src={require('./photos/foto.png')} className='contacto-img' alt={'img-contacto'}></img>
+                </div>
+                <div className='sobre'>
+                    {/* <img src={require('./photos/foto.png')} className="yo"></img> */}
+                    {/* <b>Sobre Baneto</b> */}
+
+                    <p>
+                        Modelo uno rotate.
+
+                        Modelo dos lucete.
+                    </p>
+                    <p>
+                        Me puse a hacer pendientes el famoso 2019 mientras estaba viviendo en Trento,
+                        donde el concilio. Basicamente porque me gustan mucho los pendientes
+                         y en ese momento no era muy solvente que digamos y había que buscar una alternativa.
+                    </p>
+                    <p>
+                        Salieron cosas un poco feas, pero otras no estaban nada mal. Resultó que a
+                        la gente ĺes gustaron y aquí estamos.
+                    </p>
                 </div>
             </div>
         </div>
